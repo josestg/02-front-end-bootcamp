430699c3ce07301828574925e81d8b47bd54b2ab
Jose Alfredo Sitanggang <josealfredositanggang@gmail.com>
2022-02-22 08:52:42 +0700

    done materi asyn-await

--- 14-async-await/index.js
+++ 14-async-await/index.js
@@ -20,7 +20,8 @@ function fetchDataFromServerC(dataServerB) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log("Server C");
-      resolve(3 * dataServerB);
+      //   resolve(3 * dataServerB);
+      reject(new Error("INI SYNTAX ERROR DI C"));
     }, 1000);
   });
 }
@@ -97,12 +98,60 @@ async function fetchServer() {
     const serverD = await fetchDataFromServerD(serverC);
     const serverE = await fetchDataFromServerE(serverD);
     const serverF = await fetchDataFromServerF(serverE);
-    console.log("result", serverF);
+    // throw "x";
+    return serverF;
   } catch (err) {
-    console.log("failed", err);
+    if (err instanceof Error) {
+      const serverD = await fetchDataFromServerD(10);
+      const serverE = await fetchDataFromServerE(serverD);
+      const serverF = await fetchDataFromServerF(serverE);
+      return serverF;
+    }
+
+    throw new Error("is an even number");
+    // throw new FetchAPIError(500, "is an even number");
   }
 }
 
-fetchServer();
+// class FetchAPIError extends Error {
+//   constructor(status, message) {
+//     super(message);
+//     this.status = status;
+//     this.message = message;
+//   }
+// }
+
+fetchServer()
+  .then((result) => {
+    console.log("INI RESULT", result);
+  })
+  .catch((err) =>
+    console.log(err, "apakah benaran error?", err instanceof Error)
+  );
 // 1s 0s
 fetchDataFromGoogle().then((message) => console.log(message)); // 1s exec: 0s (DONE)
+
+async function renderTodo() {
+  const ul = document.getElementById("todos");
+  try {
+    const todosResponse = await fetch(
+      "https://jsonplaceholder.typicode.com/todos",
+      {
+        method: "GET",
+      }
+    );
+
+    const todosJSON = await todosResponse.json();
+    for (let i = 0; i < todosJSON.length; i++) {
+      const title = todosJSON[i].title;
+      //   <li>Title</li>
+      const li = document.createElement("li");
+      li.textContent = title;
+      ul.appendChild(li);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+setTimeout(renderTodo, 3 * 1000);
