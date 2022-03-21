e9d346311dcbbac5bc842648d8ca7791c201e659
Jose Alfredo Sitanggang <josealfredositanggang@gmail.com>
2022-03-21 11:25:27 +0700

    migrated TodoContext to useReducer

--- react-context-part-1/src/context/TodoContext.jsx
+++ react-context-part-1/src/context/TodoContext.jsx
@@ -1,5 +1,5 @@
 import produce from "immer";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useEffect, useReducer, useState } from "react";
 import {
   createTodo,
   deleteTodo,
@@ -16,18 +16,71 @@ const initialValue = {
   updateCompleteStatus: async (id, completed) => {},
 };
 
+const SET_TODO = "set_todo";
+const ADD_TODO = "add_todo";
+const DEL_TODO = "del_todo";
+const UPT_TODO = "upt_todo";
+const SET_ERROR = "set_error";
+const START_LOADING = "start_loading";
+const STOP_LOADING = "stop_loading";
+
+const initialState = {
+  todos: [],
+  loading: true,
+  error: null,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return produce(state, (draf) => {
+        draf.todos.push(action.payload.todos);
+      });
+    case DEL_TODO:
+      return produce(state, (draf) => {
+        const index = draf.todos.findIndex(
+          (todo) => todo.id === action.payload.id
+        );
+        if (index !== -1) {
+          draf.todos.splice(index, 1);
+        }
+      });
+    case UPT_TODO:
+      return produce(state, (draf) => {
+        const index = draf.todos.findIndex(
+          (todo) => todo.id === action.payload.id
+        );
+        if (index !== -1) {
+          draf.todos[index].completed = action.payload.completed;
+        }
+      });
+    case SET_TODO:
+      return { ...state, todos: action.payload.todos };
+    case START_LOADING:
+      return { ...state, loading: true };
+    case STOP_LOADING:
+      return { ...state, loading: false };
+    case SET_ERROR:
+      return { ...state, error: action.payload.error };
+    default:
+      throw new Error("unknown action.type");
+  }
+};
+
 export const TodoContext = createContext(initialValue);
 
 const TodoProvider = (props) => {
-  const [loading, setLoading] = useState(initialValue.loading);
-  const [error, setError] = useState(initialValue.error);
-  const [todos, setTodos] = useState(initialValue.todos);
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { loading, error, todos } = state;
 
   useEffect(() => {
     retrieveTodos()
-      .then((data) => setTodos(data))
-      .catch((error) => setError(error))
-      .finally(() => setLoading(false));
+      .then((data) => dispatch({ type: SET_TODO, payload: { todos: data } }))
+      .catch((error) =>
+        dispatch({ type: SET_ERROR, payload: { error: error } })
+      )
+      .finally(() => dispatch({ type: STOP_LOADING }));
   }, []);
 
   const addTodo = async (title) => {
@@ -36,37 +89,17 @@ const TodoProvider = (props) => {
       completed: false,
     });
 
-    const nextTodos = produce(todos, (drafTodo) => {
-      drafTodo.push(createdTodo);
-    });
-
-    setTodos(nextTodos);
+    dispatch({ type: ADD_TODO, payload: { todos: createdTodo } });
   };
 
   const delTodo = async (id) => {
     await deleteTodo(id);
-
-    const nextTodos = produce(todos, (drafTodo) => {
-      const index = drafTodo.findIndex((todo) => todo.id === id);
-      if (index !== -1) {
-        drafTodo.splice(index, 1);
-      }
-    });
-
-    setTodos(nextTodos);
+    dispatch({ type: DEL_TODO, payload: { id: id } });
   };
 
   const updateCompleteStatus = async (id, completed) => {
     await updateTodo(id, completed);
-
-    const nextTodos = produce(todos, (drafTodo) => {
-      const index = drafTodo.findIndex((todo) => todo.id === id);
-      if (index !== -1) {
-        drafTodo[index].completed = completed;
-      }
-    });
-
-    setTodos(nextTodos);
+    dispatch({ type: UPT_TODO, payload: { id: id, completed: completed } });
   };
 
   const value = {
